ae336e237065abf36a6b61597ee099a21c018e5c
Simone Anthony <simone.anthony1@yahoo.com>
2022-11-11 16:40:02 -0500

    logic for inital deck created, logic for drawCard checks if the user has cards, it draws 2 if not

--- src/components/GameArena.tsx
+++ src/components/GameArena.tsx
@@ -1,24 +1,78 @@
-import React, { useState, useEffect } from 'react';
+// import axios from "axios";
+import { useState, useEffect, useCallback } from "react";
+import { fetchNewDeck, drawCards } from "../API/getRequests";
+
+interface ICard {
+  code: string;
+  image: string;
+  value: number;
+  suit: string;
+}
+
+interface ICardsInfo {
+  playerCards: {
+    cards: ICard[];
+    total: number;
+  };
+  computerCards: {
+    cards: ICard[];
+    total: number;
+  };
+}
 
 function GameArena() {
   const [playerTurn, setPlayerTurn] = useState(false);
+  const [deckId, setDeckId] = useState<null | string>(null);
+  const [cardsInfo, setCardsInfo] = useState<ICardsInfo>({
+    playerCards: {
+      cards: [],
+      total: 0,
+    },
+    computerCards: {
+      cards: [],
+      total: 0,
+    },
+  });
+
+  const drawCard = useCallback(() => {
+    if (!deckId) return;
+    // if the current player neither play has cards, draw 2 cards, else draw 1
+    const drawAppropriateNumOfCards = (currPlayerCardArr: ICard[]) =>
+      currPlayerCardArr.length === 0
+        ? drawCards(deckId, 2)
+        : drawCards(deckId, 1);
+
+    if (!playerTurn) {
+      // computer draws cards
+      drawAppropriateNumOfCards(cardsInfo.computerCards.cards);
+      setPlayerTurn(true);
+    } else {
+      // player draws cards
+      drawAppropriateNumOfCards(cardsInfo.playerCards.cards);
+    }
+  }, [
+    cardsInfo.computerCards.cards,
+    cardsInfo.playerCards.cards,
+    deckId,
+    playerTurn,
+  ]);
 
   useEffect(() => {
-    // create a deck
+    (async () => {
+      // create a deck
+      const {
+        data: { deck_id },
+      } = await fetchNewDeck();
 
-    // save the deck_id to state
+      // save the deck_id to state
+      setDeckId(deck_id);
 
-    // computer draws cards
+      // draw cards
+      drawCard();
+    })();
+  }, [drawCard]);
 
-    // player draws cards
-
-  }, []);
-  
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+  return <div className="">Hello</div>;
 }
 
 export default GameArena;
