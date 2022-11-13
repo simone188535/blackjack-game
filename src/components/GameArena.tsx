ed0cc97cbbd00ed66c20043081d1b3bc67e52587
Simone Anthony <simone.anthony1@yahoo.com>
2022-11-13 13:42:26 -0500

    code clean up

--- src/components/GameArena.tsx
+++ src/components/GameArena.tsx
@@ -81,8 +81,6 @@ function GameArena() {
   useEffect(() => {
     if (!deckId) return;
 
-    console.log("deckId", deckId);
-
     // initial cards for computer and player
     (async () => {
       // computer draws cards
