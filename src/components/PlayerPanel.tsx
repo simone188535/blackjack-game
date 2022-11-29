4b2f5f7e34fe2099e72d413f80dde28f4033ab4b
Simone Anthony <simone.anthony1@yahoo.com>
2022-11-29 15:25:50 -0500

    comments removed

--- src/components/PlayerPanel.tsx
+++ src/components/PlayerPanel.tsx
@@ -9,48 +9,12 @@ interface PlayerPanelProps {
 }
 
 function PlayerPanel({ header, cards, playerTotal, render }: PlayerPanelProps) {
-  
-//     const playerControlPanel = header === "User" && (
-//     <>
-//       <GameResults
-//         totalPlayerInfo={totalPlayerInfo}
-//         totalComputerInfo={totalComputerInfo}
-//         didPlayerStand={didPlayerStand}
-//         didPlayerWin={didPlayerWin}
-//         setDidPlayerWin={setDidPlayerWin}
-//         setTotalPlayerInfo={setTotalPlayerInfo}
-//         setTotalComputerInfo={setTotalComputerInfo}
-//       />
-//       <section className="btn-container">
-//         <button
-//           type="button"
-//           onClick={() => drawCard()}
-//           disabled={didPlayerStand || didPlayerWin !== null}
-//         >
-//           Hit
-//         </button>
-//         <button type="button" onClick={() => setDidPlayerStand(true)}>
-//           Stand
-//         </button>
-//         {/* This is reset button can be done by resetting state but I'm out of time */}
-//         <button
-//           type="button"
-//           onClick={() => {
-//             window.location.href = "/";
-//           }}
-//         >
-//           Reset
-//         </button>
-//       </section>
-//     </>
-//   );
 
   return (
     <section className="game-panel">
       <h1>{header}</h1>
       <MapCards cards={cards} />
       <div>Total: {playerTotal}</div>
-      {/* {playerControlPanel} */}
       {header === "User" && render && render()}
     </section>
   );
